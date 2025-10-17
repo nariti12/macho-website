1a6f8474cb892651307c81d0e9f8f0a98dc94934
root <root@narita.localdomain>
2025-10-17 19:15:54 +0900

    Set default site URL to production domain

--- macho-website/src/lib/seo.ts
+++ macho-website/src/lib/seo.ts
@@ -1,4 +1,4 @@
-const DEFAULT_SITE_URL = "https://macho.example";
+const DEFAULT_SITE_URL = "https://www.machoda.com";
 
 export const siteUrl = process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/$/, "") ?? DEFAULT_SITE_URL;
 
